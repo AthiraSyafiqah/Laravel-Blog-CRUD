dfa1204d766ef721a946947fae2351439b824ec2
CC18381@stdmail.ump.edu.my <CC18381@stdmail.ump.edu.my>
2020-09-11 12:25:31 +0800

    Delete function

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -4,34 +4,28 @@
 Todos List
 @endsection
 
- @section('content')
- <h1 class="text-center my-5"> TODOS PAGE </h1>
- <div class="row justify-content-center">
- <div class="row justify-content-md-2">
- <div class="card card-default">
- <div class="card-header">
-    Todos
-</div>
-<div class="card-body">
-<ul class="list-group">
+@section('content')
+  <h1 class="text-center my-5">TODOS PAGE</h1>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card card-default">
+        <div class="card-header">
+          Todos
+        </div>
 
-    @foreach($todos as $todo)
-    <li class="list-group-item">
+        <div class="card-body">
+          <ul class="list-group">
+            @foreach($todos as $todo)
+              <li class="list-group-item">
+                {{ $todo->name }}
 
-        {{ $todo -> name }}
-
-        <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right"> View </a>   
-
-    </li>
-
-    @endforeach
-
-</ul>
+                <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right">View</a>
+              </li>
+            @endforeach
+          </ul>
         </div>
       </div>
     </div>
   </div>
-</body>
-</html>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//to insert and display data
 Route::get('todos', 'TodosController@index');
 Route::get('todos/{todo}', 'TodosController@show');
 Route::get('new-todos', 'TodosController@create');
 Route::post('store-todos', 'TodosController@store');
 
+//to update data
 Route::get('todos/{todo}/edit', 'TodosController@edit');
 Route::post('todos/{todo}/update-todos', 'TodosController@update');
- 
+
+//to delete data
+Route::get('todos/{todo}/delete', 'TodosController@destroy');
 
 
